d7c5367fa60b3e7d0728811899f67e67149072c6
蓝应乐 <1506536196@qq.com>
2021-03-16 09:59:39 +0800

    empty

---
+++ 猜数字游戏/猜数字游戏/main.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void menu()
+{
+	printf("*****************************\n");
+	printf("*****1.猜数字 0.退出游戏*****\n");
+	printf("*****************************\n");
+}
+void game()
+{
+	int ret = 0;
+	ret = rand() % 100 + 1;
+	int guess = 0;
+	printf("请猜数字:\n");
+	while (1)
+	{
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了\n");
+		}
+	}
+}
+int main()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do 
+	{
+		menu();
+		printf("请选择：\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请再次选择\n");
+			break;
+		}
+	} while (input);
+}
